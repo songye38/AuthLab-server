3f8d708cc40c593cd9c01e5d06b9d705a15da7aa
songye38 <ddev0308@gmail.com>
2025-08-02 18:33:25 +0900

    UPDATE models 경로 변경

--- app/db/crud.py
+++ app/db/crud.py
@@ -7,7 +7,7 @@
 from sqlalchemy.orm import Session
 from app.db.models import User
 from passlib.context import CryptContext
-import models
+from app.db import models
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
