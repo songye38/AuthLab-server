c75518366a6dd4a04b699a8619f26edf8fda5f9c
songye38 <ddev0308@gmail.com>
2025-08-02 18:50:04 +0900

    UPDATE asyncio

--- app/redis/redis_client.py
+++ app/redis/redis_client.py
@@ -1,11 +1,10 @@
+# redis 클라이언트 세팅 (redis 4.x 이상 사용 시)
 
-# redis 클라이언트 세팅
-
-import aioredis
+import redis.asyncio as redis_async
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # 이거 꼭 해줘야 함
+load_dotenv()  # 필수
 
 REDIS_URL = os.getenv("REDIS_URL")
-redis = aioredis.from_url(REDIS_URL, encoding="utf-8", decode_responses=True)
+redis = redis_async.from_url(REDIS_URL, encoding="utf-8", decode_responses=True)
